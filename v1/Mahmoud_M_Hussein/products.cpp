ae5b2af1e52687cadc31f68f09b2b47d17806267
mahmouddmohammed <114715979+mahmouddmohammed@users.noreply.github.com>
2023-03-28 21:28:18 +0200

    update products and solve some errors

--- v1/Mahmoud_M_Hussein/products.cpp
+++ v1/Mahmoud_M_Hussein/products.cpp
@@ -8,21 +8,26 @@ void products_data();
 //enum categories;
 const int no_categories = 7;
 const int no_products = 10;
-string categories[no_categories] = {"Electronics", "VideoGames", "Groceries", "Office", "Beauty", "SportSupplies", "Home_Appliances & Furniture"};
+string categories[no_categories] = { "Electronics", "VideoGames", "Groceries", "Office", "Beauty", "SportSupplies", "Home_Appliances & Furniture" };
 string all_products[70];
 struct product {
 	string name;
 	string id;
 	string category;
-	string price;
+	double price;
 };
 struct Category {
 	product products[10];
 };
-Category Electronics, VideoGames, Groceries, Office, Beauty, SportSupplies ,HomeFurniture; // to be global
+Category Electronics, VideoGames, Groceries, Office, Beauty, SportSupplies, HomeFurniture; // to be global
 
 int main() {
-	
+	products_data();
+	view_categories();
+	string product_namee;
+	cout << "Search by the name of the product: ";
+	cin >> product_namee;
+	searchByName(product_namee);
 	return 0;
 }
 
@@ -30,103 +35,103 @@ void products_data() {
 	Electronics.products[0].name = "MSI Katana GF66 Gaming Laptop";
 	Electronics.products[0].id = "";
 	Electronics.products[0].category = "Electronics - Laptop";
-	Electronics.products[0].price = "1700$";
-	Electronics.products[1] = { "Acer Aspire 5","dasdas","Electronics - Laptop","550$" };
-	Electronics.products[2] = { "LG 55  Class 4K UHD 2160P","dasdas","Electronics - Laptop","360$" };
-	Electronics.products[3] = { "LG 65  Class 4K UHD ","dasdas","Electronics - Laptop","1300$" };
-	Electronics.products[4] = { "SAMSUNG 65 Class 4K Crystal","dasdas","Electronics - Laptop","478$" };
-	Electronics.products[5] = { "Apple Watch Series 8 GPS","dasdas","Electronics - Laptop","360$" };
-	Electronics.products[6] = { "SAMSUNG Galaxy Watch 4","dasdas","Electronics - Laptop","170$" };
-	Electronics.products[7] = { "Gateway Notebook 11.6","dasdas","Electronics - Laptop","192$" };
-	Electronics.products[8] = { "Straight Talk Apple iPhone 11, 64GB","dasdas","Electronics - Laptop","250$" };
-	Electronics.products[9] = { "Apple iPhone 12, 64GB","dasdas","Electronics - Laptop","500$" };
+	Electronics.products[0].price = 1700;
+	Electronics.products[1] = { "Acer Aspire 5","dasdas","Electronics - Laptop",550 };
+	Electronics.products[2] = { "LG 55  Class 4K UHD 2160P","dasdas","Electronics - Laptop",360 };
+	Electronics.products[3] = { "LG 65  Class 4K UHD ","dasdas","Electronics - Laptop",1300 };
+	Electronics.products[4] = { "SAMSUNG 65 Class 4K Crystal","dasdas","Electronics - Laptop",478};
+	Electronics.products[5] = { "Apple Watch Series 8 GPS","dasdas","Electronics - Laptop",360};
+	Electronics.products[6] = { "SAMSUNG Galaxy Watch 4","dasdas","Electronics - Laptop",170};
+	Electronics.products[7] = { "Gateway Notebook 11.6","dasdas","Electronics - Laptop",192};
+	Electronics.products[8] = { "Straight Talk Apple iPhone 11, 64GB","dasdas","Electronics - Laptop",250};
+	Electronics.products[9] = { "Apple iPhone 12, 64GB","dasdas","Electronics - Laptop",500};
 	//cout << Electronics.products[1].name;
 
-	VideoGames.products[0] = { "Red Dead Redemption 2","","VideoGames - PlayStation 4","20$" };
-	VideoGames.products[1] = { "FIFA 23","","VideoGames - PlayStation 4","40$" };
-	VideoGames.products[2] = { "Grand Theft Auto V","","VideoGames - PlayStation 5","20$" };
-	VideoGames.products[3] = { "Pro Evolution Soccer (PES)","","VideoGames - PlayStation 4","40$" };
-	VideoGames.products[4] = { "Call of Duty: Modern Warfare II","","VideoGames - PlayStation 5","70$" };
-	VideoGames.products[5] = { "Marvel’s Spider-Man: Miles Morales Ultimate Launch Edition","","VideoGames - PlayStation 5","70$" };
-	VideoGames.products[6] = { "Crysis Remastered Trilogy","","VideoGames - PlayStation 4","35$" };
-	VideoGames.products[7] = { "Battlefield V","","VideoGames - PlayStation 4","20$" };
-	VideoGames.products[8] = { "Resident Evil 4","","VideoGames - PlayStation 4","60$" };
-	VideoGames.products[9] = { "Hogwarts Legacy Deluxe Edition","","VideoGames - PlayStation 4","70$" };
+	VideoGames.products[0] = { "Red Dead Redemption 2","","VideoGames - PlayStation 4",20};
+	VideoGames.products[1] = { "FIFA 23","","VideoGames - PlayStation 4",40 };
+	VideoGames.products[2] = { "Grand Theft Auto V","","VideoGames - PlayStation 5",20 };
+	VideoGames.products[3] = { "Pro Evolution Soccer (PES)","","VideoGames - PlayStation 4",40 };
+	VideoGames.products[4] = { "Call of Duty: Modern Warfare II","","VideoGames - PlayStation 5",70 };
+	VideoGames.products[5] = { "Marvel’s Spider-Man: Miles Morales Ultimate Launch Edition","","VideoGames - PlayStation 5",70 };
+	VideoGames.products[6] = { "Crysis Remastered Trilogy","","VideoGames - PlayStation 4",35 };
+	VideoGames.products[7] = { "Battlefield V","","VideoGames - PlayStation 4",20 };
+	VideoGames.products[8] = { "Resident Evil 4","","VideoGames - PlayStation 4",60 };
+	VideoGames.products[9] = { "Hogwarts Legacy Deluxe Edition","","VideoGames - PlayStation 4",70 };
 
-	Groceries.products[0] = { "","","","" };
-	Groceries.products[1] = { "","","","" };
-	Groceries.products[2] = { "","","","" };
-	Groceries.products[3] = { "","","","" };
-	Groceries.products[4] = { "","","","" };
-	Groceries.products[5] = { "","","","" };
-	Groceries.products[6] = { "","","","" };
-	Groceries.products[7] = { "","","","" };
-	Groceries.products[8] = { "","","","" };
-	Groceries.products[9] = { "","","","" };
-	
-	Office.products[0] = { "","","","" };
-	Office.products[1] = { "","","","" };
-	Office.products[2] = { "","","","" };
-	Office.products[3] = { "","","","" };
-	Office.products[4] = { "","","","" };
-	Office.products[5] = { "","","","" };
-	Office.products[6] = { "","","","" };
-	Office.products[7] = { "","","","" };
-	Office.products[8] = { "","","","" };
-	Office.products[9] = { "Casio FX-991EX ","","","" };
+	Groceries.products[0] = { "Cheddar Smoked Sausage","","",3.96};
+	Groceries.products[1] = { "Freshness Guaranteed Boneless Chicken Thighs","","",11.38};
+	Groceries.products[2] = { "Freshness Guaranteed Chicken Breast Tenderloins","","",8.94 };
+	Groceries.products[3] = { "HERSHEY'S and REESE'S, Chocolate Assortment Treats","","",7.48};
+	Groceries.products[4] = { "CADBURY, MINI EGGS Milk Chocolate with a Crisp Sugar Shell","","",3.84};
+	Groceries.products[5] = { "REESE'S, Assorted Milk Chocolate","","",10.98 };
+	Groceries.products[6] = { "Sadler's Smokehouse Mesquite Smoked Sliced Beef Brisket","","",23.04};
+	Groceries.products[7] = { "Fresh Sweet Corn","","",0.60};
+	Groceries.products[8] = { "Marketside Organic Spinach","","",5.13 };
+	Groceries.products[9] = { "Sweet Potatoes","","",4.24 };
 
-	Beauty.products[0] = { "","","","" };
-	Beauty.products[1] = { "","","","" };
-	Beauty.products[2] = { "","","","" };
-	Beauty.products[3] = { "","","","" };
-	Beauty.products[4] = { "","","","" };
-	Beauty.products[5] = { "","","","" };
-	Beauty.products[6] = { "","","","" };
-	Beauty.products[7] = { "","","","" };
-	Beauty.products[8] = { "","","","" };
-	Beauty.products[9] = { "","","","" };
+	Office.products[0] = { "Vitesse Gaming Desk Computer Desk with Large Desk Mouse Pad","","", 110};
+	Office.products[1] = { "FlexiSpot Home Office Height Adjustable Standing Desk","","",250 };
+	Office.products[2] = { "SHW Cyrus 38-Inch Desk with Shelves","","", 67};
+	Office.products[3] = { "Techvida Rose gold desk organizer","","",17 };
+	Office.products[4] = { "Child Drawing Art Set / Kit with 210 Pieces","","", 20};
+	Office.products[5] = { "Pentel RSVP Ballpoint Pen","","",0.96 };
+	Office.products[6] = { "Sheet High-Security Micro-Cut Paper Credit Card Shredder","","",41 };
+	Office.products[7] = { "Bostitch Office Heavy Duty Stapler","","",11};
+	Office.products[8] = { "JIELISI 10in White Board","","", 12};
+	Office.products[9] = { "Casio FX-991EX ","","", 18};
 
-	SportSupplies.products[0] = { "","","","" };
-	SportSupplies.products[1] = { "","","","" };
-	SportSupplies.products[2] = { "","","","" };
-	SportSupplies.products[3] = { "","","","" };
-	SportSupplies.products[4] = { "","","","" };
-	SportSupplies.products[5] = { "","","","" };
-	SportSupplies.products[6] = { "","","","" };
-	SportSupplies.products[7] = { "","","","" };
-	SportSupplies.products[8] = { "","","","" };
-	SportSupplies.products[9] = { "","","","" };
+	Beauty.products[0] = { "L'Oreal Paris Glow Paradise Lip Balm","","", 8};
+	Beauty.products[1] = { "L'Oreal Paris Telescopic Lift Washable Makeup Mascara","","",12 };
+	Beauty.products[2] = { "Frozen 2 Lip Gloss","","", 5};
+	Beauty.products[3] = { "Olay Regenerist Micro-Sculpting Cream Face Moisturizer","","",30 };
+	Beauty.products[4] = { "CeraVe Foaming Face Wash","","",14 };
+	Beauty.products[5] = { "Dyson Supersonic Hair Dryer","","",270 };
+	Beauty.products[6] = { "Head & Shoulders 2 in 1 Dandruff Shampoo and Conditioner","","",12 };
+	Beauty.products[7] = { "Lottie London Cheeky Kiss Lip & Cheek Stick","","",7 };
+	Beauty.products[8] = { "Kenneth Cole Black Body Spray for Men","","",9 };
+	Beauty.products[9] = { "Brickell Men's Purifying Charcoal Face Wash for Men","","",25 };
 
-	HomeFurniture.products[0] = { "","","","" };
-	HomeFurniture.products[1] = { "","","","" };
-	HomeFurniture.products[2] = { "","","","" };
-	HomeFurniture.products[3] = { "","","","" };
-	HomeFurniture.products[4] = { "","","","" };
-	HomeFurniture.products[5] = { "","","","" };
-	HomeFurniture.products[6] = { "","","","" };
-	HomeFurniture.products[7] = { "","","","" };
-	HomeFurniture.products[8] = { "","","","" };
-	HomeFurniture.products[9] = { "","","","" };
+	SportSupplies.products[0] = { "Franklin Sports Youth Baseball Backpack","","", 19.83};
+	SportSupplies.products[1] = { "Rawlings R400 Baseball Youth Backpack","","",43 };
+	SportSupplies.products[2] = { "Rawlings Storm Girls Softball Bag","","",28 };
+	SportSupplies.products[3] = { "Easton Game Ready Baseball Backpack","","", 55};
+	SportSupplies.products[4] = { "Bounce Pro 7-Foot  Trampoline","","", 123};
+	SportSupplies.products[5] = { "Nike Phantom VSN 2 Elite","","", 272};
+	SportSupplies.products[6] = { "Nike Junior Mercurialx Proximo II","","",90 };
+	SportSupplies.products[7] = { "Muck Kid's Rugged II Boot ","","",54 };
+	SportSupplies.products[8] = { "Duo Tennis Racquet set","","",35 };
+	SportSupplies.products[9] = { "Huffy 20 Boy Kids Bike","","", 78};
+
+	HomeFurniture.products[0] = { "Platform Bed Frame with 4 Storage Drawers","","",255 };
+	HomeFurniture.products[1] = { "Allewie Brown Queen Size Bed Frame","","",189 };
+	HomeFurniture.products[2] = { "Lily's Home Hanging Wall Clock","","", 30};
+	HomeFurniture.products[3] = { "Zimtown 5 Tier Heavy Duty Steel Shelving","","", 50};
+	HomeFurniture.products[4] = { "Hoover PowerDash Cleaner","","", 81};
+	HomeFurniture.products[5] = { "Shower Curtain Christmas ","","",30};
+	HomeFurniture.products[6] = { "Costway 1700W Portable Clothes Dryer","","",330};
+	HomeFurniture.products[7] = { "BestMassage Heavy Duty Shelf ","","",43 };
+	HomeFurniture.products[8] = { "Sterilite Wide 4 Drawer","","",44.78};
+	HomeFurniture.products[9] = { "Beautiful 6 Quart Touchscreen Air Fryer","","",69 };
 
 	for (int i = 0; i < 10; i++) {
 		all_products[i] = Electronics.products[i].name;
 	}
-	for (int a = 10 , b = 0; a < 20; a++ , b++) {
+	for (int a = 10, b = 0; a < 20; a++, b++) {
 		all_products[a] = VideoGames.products[b].name;
 	}
-	for (int c = 20 , d = 0; c < 30; c++ , d++) {
+	for (int c = 20, d = 0; c < 30; c++, d++) {
 		all_products[c] = Groceries.products[d].name;
 	}
-	for (int e = 30 , f = 0; e < 40; e++ , f++) {
+	for (int e = 30, f = 0; e < 40; e++, f++) {
 		all_products[e] = Office.products[f].name;
 	}
-	for (int g = 40 ,h = 0; g < 50; g++ , h++) {
+	for (int g = 40, h = 0; g < 50; g++, h++) {
 		all_products[g] = Beauty.products[h].name;
 	}
-	for (int j = 50 , k = 0; j < 60; j++ , k++) {
+	for (int j = 50, k = 0; j < 60; j++, k++) {
 		all_products[j] = SportSupplies.products[k].name;
 	}
-	for (int l = 60 , m=0; l < 70; l++ , m++) {
+	for (int l = 60, m = 0; l < 70; l++, m++) {
 		all_products[l] = HomeFurniture.products[m].name;
 	}
 
@@ -134,7 +139,7 @@ void products_data() {
 
 void view_categories() {
 	for (int i = 0; i < no_categories; i++) {
-		cout << i << ". " << categories[i] << endl;
+		cout << i+1 << ". " << categories[i] << endl;
 	}
 	int no;
 	cout << "Choose a number: ";
@@ -147,38 +152,38 @@ void view_products(int number) {
 	switch (number) {
 	case 1:
 		for (int i = 0; i < no_products; i++) {
-			cout << i << ". " << Electronics.products[i].name << endl;
+			cout << i+1 << ". " << Electronics.products[i].name << endl;
 		}
 
 		break;
 	case 2:
 		for (int i = 0; i < no_products; i++) {
-			cout << i << ". " << VideoGames.products[i].name << endl;
+			cout << i+1 << ". " << VideoGames.products[i].name << endl;
 		}
 		break;
 	case 3:
 		for (int i = 0; i < no_products; i++) {
-			cout << i << ". " << Groceries.products[i].name << endl;
+			cout << i+1 << ". " << Groceries.products[i].name << endl;
 		}
 		break;
 	case 4:
 		for (int i = 0; i < no_products; i++) {
-			cout << i << ". " << Office.products[i].name << endl;
+			cout << i+1 << ". " << Office.products[i].name << endl;
 		}
 		break;
 	case 5:
 		for (int i = 0; i < no_products; i++) {
-			cout << i << ". " << Beauty.products[i].name << endl;
+			cout << i+1 << ". " << Beauty.products[i].name << endl;
 		}
 		break;
 	case 6:
 		for (int i = 0; i < no_products; i++) {
-			cout << i << ". " << SportSupplies.products[i].name << endl;
+			cout << i+1 << ". " << SportSupplies.products[i].name << endl;
 		}
 		break;
 	case 7:
 		for (int i = 0; i < no_products; i++) {
-			cout << i << ". " << HomeFurniture.products[i].name << endl;
+			cout << i+1 << ". " << HomeFurniture.products[i].name << endl;
 		}
 		break;
 	}

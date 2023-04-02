5521e5f415967dfd8d651db6c3d3b067e6997641
mahmouddmohammed <114715979+mahmouddmohammed@users.noreply.github.com>
2023-04-03 01:07:28 +0200

    Update final_v1.cpp

--- v1/final_v1.cpp
+++ v1/final_v1.cpp
@@ -77,6 +77,7 @@ void backto_categories(int no);
 void logout();
 void exit_program();
 void after_checkout();
+void reorder_information();
 
 int main() {
 	update_data();
@@ -1298,6 +1299,7 @@ void after_checkout() {
 	case 2:
 		system("CLS");
 		login_register_menu();
+		view_menu();
 		break;
 	case 3:
 		exit_program();
